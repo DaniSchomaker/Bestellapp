4fd583723a20bccdf09dcd6a902628a292b70c36
Dani S. <daniela.schomaker@posteo.de>
2025-09-15 16:39:34 +0200

    feat: add database

---
+++ scripts/db.js
@@ -0,0 +1,27 @@
+let myDishes = [
+  {
+    "name": "Green Bowl",
+    "price": 11.9,
+    "description": "Frische Bowl mit Avocado, Brokkoli, Grünkohlchips, Gurke, Edamame und Sesamreis, dazu Soja-Ingwer-Dressing",
+  },
+   {
+    "name": "Falafel Bowl",
+    "price": 10.9,
+    "description": "Hausgemachte Falafel, Hummus, Taboulé, Granatapfel und Gurken-Tomaten-Salat auf Bulgur",
+  },
+    {
+    "name": "Rainbow Quinoa Bowl",
+    "price": 10.9,
+    "description": "Bunter Mix aus Quinoa, Avocado, Rotkohl, Mango, Edamame und Sesam-Ingwer-Dressing",
+  },
+    {
+    "name": "Kichererbsen-Kumpel",
+    "price": 9.9,
+    "description": "Kichererbsen-Curry mit Spinat, Tomaten und Kokosmilch, serviert mit Basmatireis",
+  },
+    {
+    "name": "Soulfood Ruhrpott",
+    "price": 11.9,
+    "description": "Pasta aus roten Linsen mit cremiger Kürbis-Salbei-Sauce, dazu karamellisierte Walnüsse und Rucola",
+  }
+];
