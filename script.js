87c6fd70e958b5f495a443f0e51fdd7de300f1d8
Dani S. <daniela.schomaker@posteo.de>
2025-09-16 20:35:59 +0200

    feat: add function DishToBasket

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let basket = []; //Besser Objekt? Key: Gericht-Index, Value: Anzahl
+
+
 function renderMenuItems() {
     renderMainDishes();
     renderSideDishes();
@@ -7,8 +10,8 @@ function renderMainDishes() {
   let mainDishesRef = document.getElementById("main_dishes_details");
   mainDishesRef.innerHTML = "";
 
-  for (let indexMainDishes = 0; indexMainDishes < mainDishes.length; indexMainDishes++) {
-    mainDishesRef.innerHTML += getMainDishesTemplate(indexMainDishes);
+  for (let dishIndex = 0; dishIndex < mainDishes.length; dishIndex++) {
+    mainDishesRef.innerHTML += getMainDishesTemplate(dishIndex);
   }
 }
 
@@ -16,7 +19,19 @@ function renderSideDishes() {
   let sideDishesRef = document.getElementById("side_dishes_details");
   sideDishesRef.innerHTML = "";
 
-  for (let indexSideDishes = 0; indexSideDishes < sideDishes.length; indexSideDishes++) {
-    sideDishesRef.innerHTML += getSideDishesTemplate(indexSideDishes);
+  for (let dishIndex = 0; dishIndex < sideDishes.length; dishIndex++) {
+    sideDishesRef.innerHTML += getSideDishesTemplate(dishIndex);
   }
+}
+
+function addMainDishToBasket(dishIndex) { // in einer Funktion zusammenfassen?
+  let basketRef = document.getElementById("basket_items");
+  basketRef.innerHTML += getBasketMainDishTemplate(dishIndex);
+  // `<p>${mainDishes[dishIndex].name}</p>`;
+}
+
+function addSideDishToBasket(dishIndex) {
+  let basket = document.getElementById("basket_items");
+  basket.innerHTML += getBasketSideDishTemplate(dishIndex);
+  // `<p>${sideDishes[dishIndex].name}</p>`;
 }
