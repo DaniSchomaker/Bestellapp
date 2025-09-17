f4d25fa10209f9007cf8b8c230cd50568f7e71a5
Dani S. <daniela.schomaker@posteo.de>
2025-09-17 22:15:41 +0200

    feat: add function getBasketTotalsTemplate(subtotal, delivery)

--- scripts/template.js
+++ scripts/template.js
@@ -36,20 +36,45 @@ function getBasketMainDishTemplate(dishIndex) {
   return `
     <h3>${mainDishes[dishIndex].name}</h3>
     <div class="calc_dish_price">
-      <button>
+      <button onclick="decreaseMainDishFromBasket(${dishIndex})">
         <img src="./assets/icons/minus_orange.png" alt="Button: Anzahl um eins verringern">
       </button>  
-      <p> 1x </p>
-      <img src="./assets/icons/plus_orange.png" alt="Button: Anzahl um eins erhöhen">
-      <p> 11,90 € </p>
-      <img src="./assets/icons/trashcan_orange_pixabay.png" alt="Button: Gericht löschen">
+      <p> ${mainDishes[dishIndex].amount}x </p>
+      <button onclick="addMainDishToBasket(${dishIndex})">
+        <img src="./assets/icons/plus_orange.png" alt="Button: Anzahl um eins erhöhen">
+      </button>   
+      <p> ${(mainDishes[dishIndex].amount * mainDishes[dishIndex].price).toFixed(2)} € </p>
+      <button onclick="deleteMainDishFromBasket(${dishIndex})">
+        <img src="./assets/icons/trashcan_orange_pixabay.png" alt="Button: Gericht löschen">
+      </button>
     </div>    
-    
-    
+
     `;
   
 
 }
+
+function getBasketTotalsTemplate(subtotal, delivery) {
+  return `
+    <div class="calc_total_price">
+      <div>
+        <p>Zwischensumme:</p>
+        <p>Lieferkosten:</p>
+        <p class="total_price">Gesamt:</p>
+      </div> 
+      <div class="calc_total_price_Euro">
+        <p>${subtotal.toFixed(2)} €</p>
+        <p>5.00 €</p>
+        <p class="total_price">${(subtotal + delivery).toFixed(2)} €</p>
+      </div> 
+    </div>  
+    
+    
+  `;
+}
+
+
+
 
 function getBasketSideDishTemplate(dishIndex) {
   return `
