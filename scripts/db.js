6cfa53927d8c292af427337974d8a671da5834fa
Dani S. <daniela.schomaker@posteo.de>
2025-09-17 22:14:14 +0200

    feat: add value "amount" in db for render the basket

--- scripts/db.js
+++ scripts/db.js
@@ -3,26 +3,31 @@ let mainDishes = [
     "name": "Green Bowl",
     "price": 11.9,
     "description": "Frische Bowl mit Avocado, Brokkoli, Grünkohlchips, Gurke, Edamame und Sesamreis, dazu Soja-Ingwer-Dressing",
+    "amount": 0
   },
    {
     "name": "Falafel Bowl",
     "price": 10.9,
     "description": "Hausgemachte Falafel, Hummus, Taboulé, Granatapfel und Gurken-Tomaten-Salat auf Bulgur",
+    "amount": 0
   },
     {
     "name": "Rainbow Quinoa Bowl",
     "price": 10.9,
     "description": "Bunter Mix aus Quinoa, Avocado, Rotkohl, Mango, Edamame und Sesam-Ingwer-Dressing",
+    "amount": 0
   },
     {
     "name": "Kichererbsen-Kumpel",
     "price": 9.9,
     "description": "Kichererbsen-Curry mit Spinat, Tomaten und Kokosmilch, serviert mit Basmatireis",
+    "amount": 0
   },
     {
     "name": "Soulfood Ruhrpott",
     "price": 11.9,
     "description": "Pasta aus roten Linsen mit cremiger Kürbis-Salbei-Sauce, dazu karamellisierte Walnüsse und Rucola",
+    "amount": 0
   }
 ];
 
@@ -31,16 +36,19 @@ let sideDishes = [
   {
     "name": "Süßkartoffel-Pommes",
     "price": 4.5,
-    "description": "Knusprige Süßkartoffel-Pommes mit hausgemachter Limetten-Aioli"
+    "description": "Knusprige Süßkartoffel-Pommes mit hausgemachter Limetten-Aioli",
+    "amount": 0
   },
   {
     "name": "Ofengemüse Pottstyle",
     "price": 4.9,
-    "description": "Bunte Mischung aus Ofengemüse der Saison mit frischen Kräutern"
+    "description": "Bunte Mischung aus Ofengemüse der Saison mit frischen Kräutern",
+    "amount": 0
   },
   {
     "name": "Grünkohl-Chips",
     "price": 3.5,
-    "description": "Knusprige Grünkohlchips, leicht gesalzen – der gesunde Snackklassiker"
+    "description": "Knusprige Grünkohlchips, leicht gesalzen – der gesunde Snackklassiker",
+    "amount": 0
   }
 ];
