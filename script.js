4b900bbda2d23f6bfeff44b9ddcdbb4e6ddb452d
Dani S. <daniela.schomaker@posteo.de>
2025-09-18 14:56:43 +0200

    feat: change basket rendering to show newly added dishes at the bottom, remove sideDishes

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let basket = []; // raus?
+let basket = []; 
 
 function init() {
   renderMainDishes();
-  renderSideDishes();
   renderBasket();
 }
 
@@ -15,66 +14,54 @@ function renderMainDishes() {
   }
 }
 
-function renderSideDishes() {
-  let sideDishesRef = document.getElementById("side_dishes_details");
-  sideDishesRef.innerHTML = "";
-
-  for (let dishIndex = 0; dishIndex < sideDishes.length; dishIndex++) {
-    sideDishesRef.innerHTML += getSideDishesTemplate(dishIndex);
-  }
-}
-
-// function addMainDishToBasket(dishIndex) { // in einer Funktion zusammenfassen?
-//   let basketRef = document.getElementById("basket_items");
-//   basketRef.innerHTML += getBasketMainDishTemplate(dishIndex);
-// }
-
 function addMainDishToBasket(dishIndex) {
+  if (mainDishes[dishIndex].amount === 0) {
+    basket.push(dishIndex); // Wenn das Gericht erstmalig in den Warenkorb kommt, wird es als letztes Element in das basket-Array gepusht (Reihenfolge)
+  }
   mainDishes[dishIndex].amount++;
-  console.log(mainDishes[dishIndex].amount); // später raus
-
   renderBasket();
 }
 
 function decreaseMainDishFromBasket(dishIndex) {
   mainDishes[dishIndex].amount--;
-  console.log(mainDishes[dishIndex].amount); // später raus
 
+  if (mainDishes[dishIndex].amount === 0) {
+    // Das Gericht wird aus dem basket-Array entfernt, hierfür muss ich wissen an WELCHER STELLE das Gericht im basket-Array steht:
+    let position = basket.indexOf(dishIndex); // indexOf(dishIndex) für: An welcher Stelle steht dieser dishIndex im basket-Array?
+    if (position !== -1) {
+      // position !== -1: Eintrag wurde gefunden (wenn der Eintrag nicht im basket-Array enthalten ist, wird -1 ausgegeben)
+      basket.splice(position, 1); // lösche genau EIN Element des Arrays an der entsprechenden POSITION
+    }
+  }
   renderBasket();
 }
 
 function deleteMainDishFromBasket(dishIndex) {
-  mainDishes[dishIndex].amount = 0;
-  console.log(mainDishes[dishIndex].amount); // später raus
-
+  let position = basket.indexOf(dishIndex); 
+  if (position !== -1) {
+    basket.splice(position, 1); // Gericht wird aus dem basket-Array entfernt
+  }
+  mainDishes[dishIndex].amount = 0; 
   renderBasket();
 }
 
 function renderBasket() {
+  let delivery = 5;
+  let subtotal = 0;
   let basketRef = document.getElementById("basket_items");
   basketRef.innerHTML = "";
 
-  let subtotal = 0;
-  let delivery = 5;
-
-  for (let dishIndex = 0; dishIndex < mainDishes.length; dishIndex++) {
-    if (mainDishes[dishIndex].amount > 0) {
-      basketRef.innerHTML += getBasketMainDishTemplate(dishIndex);
-      subtotal += mainDishes[dishIndex].amount * mainDishes[dishIndex].price;
-    }
-  }
-
-  if (subtotal === 0) {
-    basketRef.innerHTML = `<p class="empty_basket">Pott leer, Gemüse her!</p>`;
+  if (basket.length === 0) {
+    basketRef.innerHTML = `<p class="empty_basket">Pott leer, Gemüse her!</p>`; // Template?
     return;
   }
 
+  for (let basketIndex = 0; basketIndex < basket.length; basketIndex++) {
+    let dishIndex = basket[basketIndex]; // Holt aus dem basket-Array an der Position basketIndex den gespeicherten Wert (Index eines Gerichts im mainDishes-Array) und speichert ihn in der Variable dishIndex
+
+    basketRef.innerHTML += getBasketMainDishTemplate(dishIndex);
+    subtotal += mainDishes[dishIndex].amount * mainDishes[dishIndex].price;
+
+  }
   basketRef.innerHTML += getBasketTotalsTemplate(subtotal, delivery);
-}
-
-
-
-function addSideDishToBasket(dishIndex) {
-  let basket = document.getElementById("basket_items");
-  basket.innerHTML += getBasketSideDishTemplate(dishIndex);
 }
