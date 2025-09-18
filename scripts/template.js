e51f0caa7f5b4090e0b5961905df32a0e3c70fb6
Dani S. <daniela.schomaker@posteo.de>
2025-09-18 14:56:51 +0200

    feat: remove sideDishes

--- scripts/template.js
+++ scripts/template.js
@@ -15,21 +15,6 @@ function getMainDishesTemplate(dishIndex) {
   `;
 }
 
-function getSideDishesTemplate(dishIndex) {
-  return `
-    <article class="single_dish">  
-      <div>  
-        <h3>${sideDishes[dishIndex].name}</h3>
-        <p>${sideDishes[dishIndex].description}</p>
-        <p class="price">${sideDishes[dishIndex].price.toFixed(2)} €</p>
-      </div>
-      <button class="add_to_basket_button" onclick="addSideDishToBasket(${dishIndex})"> 
-        <img src="./assets/icons/plus_orange.png" alt="Button: Beilage zum Warenkorb hinzufügen">
-      </button>  
-    </article>
-  `;
-}
-
 ////// Basket //////
 
 function getBasketMainDishTemplate(dishIndex) {
@@ -48,10 +33,7 @@ function getBasketMainDishTemplate(dishIndex) {
         <img src="./assets/icons/trashcan_orange_pixabay.png" alt="Button: Gericht löschen">
       </button>
     </div>    
-
     `;
-  
-
 }
 
 function getBasketTotalsTemplate(subtotal, delivery) {
@@ -67,26 +49,6 @@ function getBasketTotalsTemplate(subtotal, delivery) {
         <p>5.00 €</p>
         <p class="total_price">${(subtotal + delivery).toFixed(2)} €</p>
       </div> 
-    </div>  
-    
-    
-  `;
-}
-
-
-
-
-function getBasketSideDishTemplate(dishIndex) {
-  return `
-    <h3>${sideDishes[dishIndex].name}</h3>
-        <div class="calc_dish_price">
-      <button>
-        <img src="./assets/icons/minus_orange.png" alt="Button: Anzahl um eins verringern">
-      </button>  
-      <p> 1x </p>
-      <img src="./assets/icons/plus_orange.png" alt="Button: Anzahl um eins erhöhen">
-      <p> 11,90 € </p>
-      <img src="./assets/icons/trashcan_orange_pixabay.png" alt="Button: Gericht löschen">
-    </div>    
+    </div>      
   `;
 }
